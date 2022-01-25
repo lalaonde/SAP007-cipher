81e9252ab11eca2261056cc498bee0198a45b5ed
Layssa Aragão <oilayssaa@gmail.com>
2022-01-25 16:08:58 -0300

    updates 24/01

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,5 @@ const cipher = {
 };
 
 export default cipher;
+
+// isso ai em cima ja veio no arquivo, nao mexer
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+
+//const btnCodificar = document.getElementById('btn-cifrar');
+
+ 
+function cifrarMensagem(texto) {
+    var display = texto
+    alert(display)
+}   
+
+
+/* document.getElementById("btn-cifrar").addEventListener("click", function() {
+    document.getElementById("msg-confirmacao").innerHTML = "Hello, World!";
+});
+*/ 
+
