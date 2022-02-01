a04affe7b4f3091a73f72cba712d1a56b342b67e
Layssa Aragão <oilayssaa@gmail.com>
2022-02-01 11:15:58 -0300

    separei os arquivos js. DOM funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,79 @@
+const alfabeto = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 const cipher = {
-  // ...
+  encode: function (offset, mensagem) {
+    const mensagemParaCifrar = mensagem.toUpperCase()
+      .replace(/\s+/g, "");
+    
+    let resultadoCifra = "";
+
+    for (var i = 0; i < mensagemParaCifrar.length; i++) {
+      let novoIndexLetraCifrada =
+        parseInt(alfabeto.indexOf(mensagemParaCifrar[i])) + offset;
+
+      let letraCifradaEVoltaAlfabeto = alfabeto[novoIndexLetraCifrada % 26];
+
+      if (resultadoCifra == "") {
+        resultadoCifra = letraCifradaEVoltaAlfabeto;
+      } else {
+        resultadoCifra = resultadoCifra + letraCifradaEVoltaAlfabeto;
+      }
+    }
+    alert(resultadoCifra);
+  },
+
+  decode: function (offset, mensagem) {
+    let mensagemParaDecifrar = mensagem.toUpperCase()
+      .replace(/\s+/g, ""); //prompt("Digite seu nome:");
+  
+    let resultadoDecifrar = "";
+    let novoIndexLetraDecifrada;
+    let letraDecifradaEVoltaAlfabeto;
+
+    for (var i = 0; i < mensagemParaDecifrar.length; i++) {
+      novoIndexLetraDecifrada =
+        parseInt(alfabeto.indexOf(mensagemParaDecifrar[i])) - offset;
+
+      if (novoIndexLetraDecifrada >= 0) {
+        letraDecifradaEVoltaAlfabeto = alfabeto[novoIndexLetraDecifrada];
+      } else {
+        letraDecifradaEVoltaAlfabeto = alfabeto[novoIndexLetraDecifrada + 26];
+      }
+
+      if (resultadoDecifrar == "") {
+        resultadoDecifrar = letraDecifradaEVoltaAlfabeto;
+      } else {
+        resultadoDecifrar = resultadoDecifrar + letraDecifradaEVoltaAlfabeto;
+      }
+    }
+    alert(resultadoDecifrar);
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,103 +1,33 @@
-// import cipher from './cipher.js';   <-- TEM QUE TER ISSO - PRECISO DIVIDIR OS ARQUIVOS
+import cipher from './cipher.js';   
 
 // var button = document.querySelector("button");
 
-const alfabeto = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+
 
 // ao apertar o botao - os eventos dentro da function sao disparados
 
-var cifrar = document.getElementById("btn-cifrar"); 
-    cifrar.addEventListener("click", cifrarTexto);
 
 
-function cifrarTexto() {
-  let mensagemParaCifrar = document.getElementById("cifrar").value.toUpperCase().replace(/\s+/g, '');
-   //   mensagemParaCifrar.replace(/\s+/g, '');
-  // mensagemParaCifrar.toUpperCas
-       // tentando remover os espacos - estao virando a letra relativa ao offset (ex:offset 3 - letra C)
-  var offsetCifra = document.getElementById("offset").value; //prompt("Escolha o deslocamento:");
-  var resultadoCifra = "";
+let cifrar = document.getElementById("btn-cifrar");
+cifrar.addEventListener("click", function(){
+  let mensagemParaCifrar = document
+      .getElementById("cifrar")
+      .value;
+  let offset = document.getElementById("offset").valueAsNumber;
+  cipher.encode(offset, mensagemParaCifrar)
 
-  for (var i = 0; i < mensagemParaCifrar.length; i++) {
-    var novoIndexLetraCifrada =
-      parseInt(alfabeto.indexOf(mensagemParaCifrar[i])) + parseInt(offsetCifra);
 
-    var letraCifradaEVoltaAlfabeto = alfabeto[novoIndexLetraCifrada % 26];
-    // resultadoCifra = letraCifradaEVoltaAlfabeto;
+});
 
-    if (resultadoCifra == "") {
-      resultadoCifra = letraCifradaEVoltaAlfabeto;
-    } else {
-      resultadoCifra = resultadoCifra + letraCifradaEVoltaAlfabeto;
+let decifrar = document.getElementById("btn-decifrar");
+decifrar.addEventListener("click", function(){
+  let mensagemParaCifrar = document
+  .getElementById("decifrar")
+  .value;
+let offset = document.getElementById("decifra-offset").valueAsNumber;
+cipher.decode(offset, mensagemParaCifrar)
 
-      //alert(resultadoCifra)
-    }
-  }
-  alert(resultadoCifra);
-}
 
-var decifrar = document.getElementById("btn-decifrar");
-decifrar.addEventListener("click", decifrarTexto);
 
-function decifrarTexto() {
-  var mensagemParaDecifrar = document.getElementById("decifrar").value.toUpperCase().replace(/\s+/g, ''); //prompt("Digite seu nome:");
-  var offsetDecifrar = document.getElementById("decifra-offset").value; //prompt("Escolha o deslocamento:");
-  var resultadoDecifrar = "";
-  var novoIndexLetraDecifrada;
-  var letraDecifradaEVoltaAlfabeto;
+});
 
-  for (var i = 0; i < mensagemParaDecifrar.length; i++) {
-    novoIndexLetraDecifrada =
-      parseInt(alfabeto.indexOf(mensagemParaDecifrar[i])) -
-      parseInt(offsetDecifrar);
-
-    if (novoIndexLetraDecifrada >= 0) {
-      letraDecifradaEVoltaAlfabeto = alfabeto[novoIndexLetraDecifrada];
-
-      // alert(novoIndexLetraDecifrada); // isso mostra a posição
-      //  alert(letraDecifradaEVoltaAlfabeto);
-    } else {
-      letraDecifradaEVoltaAlfabeto = alfabeto[novoIndexLetraDecifrada + 26];
-      // alert(alfabetoReverso); isso mostra a posição
-
-      // var letraAlfabetoReverso = alfabeto[alfabetoReverso];
-
-      //   alert(letraAlfabetoReverso);
-    }
-
-    if (resultadoDecifrar == "") {
-      resultadoDecifrar = letraDecifradaEVoltaAlfabeto;
-    } else {
-      resultadoDecifrar = resultadoDecifrar + letraDecifradaEVoltaAlfabeto;
-
-    }
-  } 
-  alert(resultadoDecifrar);
-}
