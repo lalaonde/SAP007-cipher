2a03fdf8de1309603ee687be5004be931838d5ae
Layssa Aragão <oilayssaa@gmail.com>
2022-02-03 14:39:11 -0300

    resultados cifra e decifra retornando para o txtarea e css changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let cifrar = document.getElementById("btn-cifrar");
 cifrar.addEventListener("click", function () {
   let mensagemParaCifrar = document.getElementById("cifrar").value;
   let offset = document.getElementById("offset").valueAsNumber;
-  document.getElementById("cifrar").value=cipher.encode(offset, mensagemParaCifrar);
+  document.getElementById("decifrar").value=cipher.encode(offset, mensagemParaCifrar);
   
 });
 
@@ -12,5 +12,5 @@ let decifrar = document.getElementById("btn-decifrar");
 decifrar.addEventListener("click", function () {
   let mensagemParaCifrar = document.getElementById("decifrar").value;
   let offset = document.getElementById("decifra-offset").valueAsNumber;
-  document.getElementById("decifrar").value=cipher.decode(offset, mensagemParaCifrar);
+  document.getElementById("cifrar").value=cipher.decode(offset, mensagemParaCifrar);
 });
